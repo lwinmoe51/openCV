ff0aa2b0e23f7dc6831b3c0442d873dd9cae433a
Lwin Moe <lwinmoe969786@gmail.com>
2021-10-09 16:10:46 +0630

    add error info

--- openCV python/video/playingVideoFromFile/playingVideoFromFile.py
+++ openCV python/video/playingVideoFromFile/playingVideoFromFile.py
@@ -1,7 +1,20 @@
 import numpy as np
 import cv2 as cv
 
+#cap = cv.VideoCapture('testNS.mp4')  #this line doesn't work. I think it may be path problem
 cap = cv.VideoCapture('E:/MyPractiseZone/openCV/openCV python/video/video_file/testNS.mp4')
+
+#Below commented code are not work . It cause isOpened() function always return false.
+#if not cap.isOpened():
+    #print("Cannot open camera")
+    #exit()
+
+#while(cap.isOpened):
+    #isTrue,frame = cap.read()
+    #cv.imshow('Video', frame)
+
+    #if cv.waitKey(25) & 0xFF==ord('d'):
+        #break
 
 while True:
     isTrue,frame = cap.read()
